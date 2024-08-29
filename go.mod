73d7945899bf5203998855659e97ad2fefbf30e5
xeonds <xeonds@stu.xidian.edu.cn>
2024-08-29 16:07:52 +0800

    feat: add backend stun server

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module aether-db
 
 go 1.23.0
 
-require github.com/gorilla/websocket v1.5.3
+require (
+	github.com/gorilla/websocket v1.5.3
+	gortc.io/stun v1.23.0
+)
--- main.go
+++ main.go
@@ -1,12 +1,13 @@
-// Example: Go WebSocket server for hole punching
 package main
 
 import (
 	"log"
+	"net"
 	"net/http"
 	"sync"
 
 	"github.com/gorilla/websocket"
+	"gortc.io/stun"
 )
 
 var upgrader = websocket.Upgrader{
@@ -15,6 +16,7 @@ var upgrader = websocket.Upgrader{
 		return true
 	},
 }
+
 var clients = make(map[*websocket.Conn]bool)
 var mutex = &sync.Mutex{}
 
@@ -54,7 +56,60 @@ func handleConnection(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func startStunServer() {
+	// Listen on UDP port 3478, the default STUN port
+	conn, err := net.ListenPacket("udp", ":3478")
+	if err != nil {
+		log.Fatal("Failed to start STUN server:", err)
+	}
+	defer conn.Close()
+	log.Println("STUN server started on port 3478")
+
+	for {
+		buf := make([]byte, 1500)
+		_, addr, err := conn.ReadFrom(buf)
+		if err != nil {
+			log.Println("Error reading from connection:", err)
+			continue
+		}
+
+		message := new(stun.Message)
+		err = message.Decode()
+		if err != nil {
+			log.Println("Failed to decode STUN message:", err)
+			continue
+		}
+
+		// Handle binding requests
+		if message.Type.Class == stun.ClassRequest && message.Type.Method == stun.MethodBinding {
+			response := stun.New()
+			response.Type = stun.NewType(stun.MethodBinding, stun.ClassSuccessResponse)
+			response.TransactionID = message.TransactionID
+
+			// Add the XOR-MAPPED-ADDRESS attribute
+			xorAddr := &stun.XORMappedAddress{
+				IP:   addr.(*net.UDPAddr).IP,
+				Port: addr.(*net.UDPAddr).Port,
+			}
+			xorAddr.AddTo(response)
+
+			// Encode and send the response
+			response.Encode()
+			_, err = conn.WriteTo(response.Raw, addr)
+			if err != nil {
+				log.Println("Failed to send STUN response:", err)
+			}
+		}
+	}
+}
+
 func main() {
+	// Start WebSocket server
 	http.HandleFunc("/ws", handleConnection)
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	go func() {
+		log.Fatal(http.ListenAndServe(":8080", nil))
+	}()
+
+	// Start STUN server
+	startStunServer()
 }
